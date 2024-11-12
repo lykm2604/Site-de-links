bed6208d1d49120ff5a6bc34c614235a7d6bc69b
“Lucas” <lykm2604@gmail.com>
2024-11-12 14:42:18 -0300

    Correção da foto e ajuste no js

--- script.js
+++ script.js
@@ -1,12 +1,4 @@
 function mudarTema() {
   const html = document.documentElement
   html.classList.toggle("light")
-
-  const img = document.querySelector("#perfil img")
-
-  if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar-light.png")
-  } else {
-    img.setAttribute("src", "./assets/avatar.png")
-  }
 }
